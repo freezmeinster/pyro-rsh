a13a1ec8e2f52e54ed73b2af680f4afdab026ff4
unknown <Didi_Erni@.(none)>
2010-07-03 08:45:51 +0700

    Comment dikit

--- client.py
+++ client.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+########################
+# Modify By Freez Meinster
+#
 import Pyro.core
 nguk = Pyro.core.getProxyForURI("PYROLOC://localhost:7766/sh")
 print "Input your command:"
